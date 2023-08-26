ef83792511764b50ca2181721fe2edafeb3a7dc8
Michał Viwegier <beebas@wp.pl>
2023-08-26 13:22:03 +0200

    Task 1 of 3 #9 done.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodySection = document.querySelector('body');
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.disabled = 1;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = 1;
+  stopBtn.disabled = 0;
+  timerId = setInterval(() => {
+    bodySection.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = 0;
+  stopBtn.disabled = 1;
+  clearInterval(timerId);
+  timerId = null;
+});
